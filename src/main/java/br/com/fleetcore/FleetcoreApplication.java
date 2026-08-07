c2673ffd2351958af44bb123a33f1b9313444d82
Joaze Francisco <joazefrancisco@gmail.com>
2026-08-07 12:18:40 -0300

    chore: initialize FleetCore project with Spring Boot

---
+++ src/main/java/br/com/fleetcore/FleetcoreApplication.java
@@ -0,0 +1,12 @@
+package br.com.fleetcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetcoreApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/fleetcore/FleetcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fleetcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
