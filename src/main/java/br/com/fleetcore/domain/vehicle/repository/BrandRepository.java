a6902b4e0468fc56b6975a2e7afc9b993e505f6d
Joaze Francisco <joazefrancisco@gmail.com>
2026-09-17 13:21:28 -0300

    fix: add Repository annotation to BrandRepository

--- src/main/java/br/com/fleetcore/domain/vehicle/repository/BrandRepository.java
+++ src/main/java/br/com/fleetcore/domain/vehicle/repository/BrandRepository.java
@@ -4,7 +4,9 @@ import br.com.fleetcore.domain.vehicle.entity.Brand;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BrandRepository extends JpaRepository<Brand, Long> {
 
     boolean existsByNameIgnoreCase(String name);
